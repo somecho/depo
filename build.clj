6045265a7da2a02900d4a567908a04bf0d7bd705
Somē Cho <itssomicho@gmail.com>
2023-07-02 09:09:43 +0200

    update build script

--- build.clj
+++ build.clj
@@ -6,6 +6,8 @@
 (def version "0.0.8")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 (def class-dir "target/classes")
+(def url "https://github.com/somecho/depo.git")
+(def connection (str "scm:git:" url))
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -15,6 +17,9 @@
                 :lib lib
                 :version version
                 :basis (b/create-basis {:project "deps.edn"})
+                :scm {:connection connection
+                      :developerConnection connection
+                      :url url}
                 :src-dirs ["src"]})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
