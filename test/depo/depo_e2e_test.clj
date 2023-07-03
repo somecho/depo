afc2efeaea46e8b648edf574c38c757efb32663e
Somē Cho <itssomicho@gmail.com>
2023-07-03 23:08:27 +0200

    dynamically create e2e tests

--- test/depo/depo_e2e_test.clj
+++ test/depo/depo_e2e_test.clj
@@ -1,11 +1,16 @@
 (ns depo.depo-e2e-test
-  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+  (:require [clojure.test :refer [testing is use-fixtures]]
             [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]))
 
 (def INPUT-FOLDER "test/resources/input/")
 (def TEMP-FOLDER "test/resources/temp/")
 (def SNAPSHOTS-FOLDER "test/resources/snapshots/")
+(def FILES ["bb.edn" "shadow-cljs.edn" "deps.edn" "project.clj"])
+(def tests [{:name "add-reagent-1.2.0"
+             :commands ["add" "reagent@1.2.0"]}
+            {:name "add-multiple"
+             :commands ["add" "reagent@1.2.0" "org.clojure/java.jdbc@0.7.12"]}])
 
 (defn delete-directory
   "Recursively delete a directory."
@@ -14,12 +19,26 @@
     (run! delete-directory (.listFiles file)))
   (io/delete-file file))
 
-(defn before-after [f]
-  (io/make-parents "test/resources/temp/FILE")
-  (f)
-  (delete-directory (io/file "test/resources/temp")))
+(defn setup-directories []
+  (for [{:keys [name]} tests]
+    (io/make-parents (str "test/resources/temp/" name "/FILE"))))
 
-(use-fixtures :each before-after)
+(defn delete-directories []
+  (for [{:keys [name]} tests]
+    (delete-directory (io/file (str "test/resources/temp/" name "/FILE")))))
+
+(defn before-after [f]
+  (setup-directories)
+  (f)
+  (delete-directories))
+
+(use-fixtures :once before-after)
+
+(defn add-test
+  "Add a test to the given namespace. The body of the test is given as
+  the thunk test-fn. Useful for adding dynamically generated deftests."
+  [name ns test-fn & [metadata]]
+  (intern ns (with-meta (symbol name) (merge metadata {:test #(test-fn)})) (fn [])))
 
 (defn apply-compare
   [filename target-folder & commands]
@@ -30,24 +49,13 @@
     (apply sh (concat ["clojure" "-M" "-m" "depo.core" "-f" target] commands))
     (= (slurp snapshot) (slurp target))))
 
-(deftest add-multiple
-  (io/make-parents (str TEMP-FOLDER "add-multiple/FILE"))
-  (testing "project.clj"
-    (is (apply-compare "project.clj" "add-multiple" "add" "reagent@1.2.0" "org.clojure/java.jdbc@0.7.12")))
-  (testing "deps.edn"
-    (is (apply-compare "deps.edn" "add-multiple" "add" "reagent@1.2.0" "org.clojure/java.jdbc@0.7.12")))
-  (testing "shadow-cljs.edn"
-    (is (apply-compare "shadow-cljs.edn" "add-multiple" "add" "reagent@1.2.0" "org.clojure/java.jdbc@0.7.12")))
-  (testing "bb.edn"
-    (is (apply-compare "bb.edn" "add-multiple" "add" "reagent@1.2.0" "org.clojure/java.jdbc@0.7.12"))))
-
-(deftest add-reagent-1.2.0
-  (io/make-parents (str TEMP-FOLDER "add-reagent-1.2.0/FILE"))
-  (testing "project.clj"
-    (is (apply-compare "project.clj" "add-reagent-1.2.0" "add" "reagent@1.2.0")))
-  (testing "deps.edn"
-    (is (apply-compare "deps.edn" "add-reagent-1.2.0" "add" "reagent@1.2.0")))
-  (testing "shadow-cljs.edn"
-    (is (apply-compare "shadow-cljs.edn" "add-reagent-1.2.0" "add" "reagent@1.2.0")))
-  (testing "bb.edn"
-    (is (apply-compare "bb.edn" "add-reagent-1.2.0"  "add" "reagent@1.2.0"))))
+(doall
+ (for [{:keys [name commands]} tests]
+   (do
+     (io/make-parents (str TEMP-FOLDER name "/FILE"))
+     (add-test name
+               'depo.depo-e2e-test
+               #(doall
+                 (for [f FILES]
+                   (testing f
+                     (is (apply apply-compare (concat [f name] commands))))))))))
