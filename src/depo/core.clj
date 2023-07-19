6ba0457ab303ec4fbbd946541ea1e4bbcb64ece7
Somē Cho <itssomicho@gmail.com>
2023-07-19 10:07:42 +0200

    0.4.34 - documentation and scripts

--- src/depo/core.clj
+++ src/depo/core.clj
@@ -44,7 +44,7 @@
 (def CONFIGURATION
   {:command "depo"
    :description "Manage dependencies for Clojure projects easily"
-   :version "0.4.33"
+   :version "0.4.34"
    :opts [{:as "path to configuration file"
            :default nil
            :option "file"
